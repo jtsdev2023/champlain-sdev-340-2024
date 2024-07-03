dc8eace82cf0470bc702e15e586a2c049d3664aa
jtsdev2023 <jtsdev2023@gmail.com>
2024-07-03 11:16:44 +0000

    working on week 1 assignment

--- week-1-assignment.cc
+++ week-1-assignment.cc
@@ -6,13 +6,15 @@ class Car {
 private:
     std::string make;
     std::string model;
+    std::string edition;
     int year;
 
 public:
     // Constructor
-    Car(std::string make, std::string model, int year) {
+    Car(std::string make, std::string model, std::string edition, int year) {
         this->make = make;
         this->model = model;
+        this->edition = edition;
         this->year = year;
     }
 
@@ -31,21 +33,52 @@ public:
         return year;
     }
 
+    // Method to turn on the ignition
+    void carIgnition(bool ignition) {
+        switch (ignition) {
+            case true:
+                std::cout << "Ignition is on." << std::endl;
+                break;
+            case false:
+                std::cout << "Ignition is off." << std::endl;
+                break;
+        }
+    }
+
+    // Method for automatic transmission
+    void automaticTransmission(char gear) {
+        gear = toupper(gear);
+        switch (gear) {
+            case 'P':
+                std::cout << "Automatic transmission is in 'PARK'." << std::endl;
+                break;
+            case 'N':
+                std::cout << "Automatic transmission is in 'NEUTRAL'." << std::endl;
+                break;
+            case 'D':
+                std::cout << "Automatic transmission is in 'DRIVE'." << std::endl;
+                break;
+        }
+    }
+
     // Display car information
     void displayInfo() const {
-        std::cout << "Car Information: " << std::endl;
+        std::cout << "Vehicle Information: " << std::endl;
         std::cout << "Make: " << make << std::endl;
         std::cout << "Model: " << model << std::endl;
+        std::cout << "Edition: " << edition << std::endl;
         std::cout << "Year: " << year << std::endl;
     }
 };
 
 int main() {
     // Create a Car object
-    Car myCar("Toyota", "Camry", 2022);
+    Car myCar("Jeep", "Wrangler", "JK 392", 2020);
 
     // Display car information
     myCar.displayInfo();
+    myCar.carIgnition(true);
+    myCar.automaticTransmission('d');
 
     return 0;
 }
